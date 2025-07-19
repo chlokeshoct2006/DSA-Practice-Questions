d8544a1ccdcb4d68b70b26741b99111e73a8f927
Lokesh Chaudhary <chlokeshoct2006@gmail.com>
2025-07-19 11:34:33 +0530

    new file added

---
+++ R_print1ToN.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class R_print1ToN {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Enter a number: "); 
+        int n = scanner.nextInt(); 
+        print(n);
+    }
+    public static void print(int n){
+        if(n==0){
+            return;
+        }
+        print(n-1);
+        System.out.println(n);
+    }
+}
