17bd3bcdc5c55fb65369d0dd005387b7ebb58aa3
Lokesh Chaudhary <156283625+chlokeshoct2006@users.noreply.github.com>
2026-06-16 10:08:56 +0530

    Create 540-SingleElementInASortedArray

---
+++ 540-SingleElementInASortedArray/540-SingleElementInASortedArray.java
@@ -0,0 +1,14 @@
+// Last updated: 6/16/2026, 10:08:51 AM
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        if(nums.length==1){
+            return nums[0];
+        }
+        for(int i=0; i<nums.length-1; i+=2){
+            if(nums[i]!=nums[i+1]){
+                return nums[i];   
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
