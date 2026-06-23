d5fc3cb3d3471ba94167dd61820bd6d11de36957
Lokesh Chaudhary <156283625+chlokeshoct2006@users.noreply.github.com>
2026-06-23 13:23:02 +0530

    File updated at 6/23/2026, 1:22:54 PM

--- 35-SearchInsertPosition/35-SearchInsertPosition.java
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -1,17 +1,15 @@
-// Last updated: 6/23/2026, 1:00:07 PM
+// Last updated: 6/23/2026, 1:22:54 PM
 1class Solution {
-2    public int searchInsert(int[] nums, int target) {
-3        int left = 0, right = nums.length-1;
-4        while(left<=right){
-5            int mid = (left+right)/2;
-6            if(nums[mid]==target) return mid;
-7            else if(nums[mid]<target){
-8                left = mid+1;
-9            }
-10            else{
-11                right = mid-1;
-12            }
-13        }
-14        return left;
-15    }
-16}
+2    public int findPeakElement(int[] nums) {
+3        int l = 0, r = nums.length - 1;
+4        while (l < r) {
+5            int mid = (l + r) / 2;
+6            if (nums[mid] > nums[mid + 1]) {
+7                r = mid;
+8            } else {
+9                l = mid + 1;
+10            }
+11        }
+12        return l;
+13    }
+14}
