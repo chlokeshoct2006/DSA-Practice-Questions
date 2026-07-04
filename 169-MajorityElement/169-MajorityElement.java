f7be4718fd83f14bc7cd807ccf845369372af95c
Lokesh Chaudhary <156283625+chlokeshoct2006@users.noreply.github.com>
2026-07-04 09:32:41 +0530

    File updated at 7/4/2026, 9:32:30 AM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,18 +1,18 @@
-// Last updated: 6/16/2026, 10:09:10 AM
-class Solution {
-    public int majorityElement(int[] nums) {
-        int candidate=0,count=0;
-        for(int i=0; i<nums.length; i++){
-            if(count==0){
-                candidate = nums[i];
-            }
-            if(nums[i]==candidate){
-                count++;
-            }
-            else{
-                count--;
-            }
-        }
-        return candidate;
-    }
-}
+// Last updated: 7/4/2026, 9:32:30 AM
+1class Solution {
+2    public int majorityElement(int[] nums) {
+3        int candidate=0,count=0;
+4        for(int i=0; i<nums.length; i++){
+5            if(count==0){
+6                candidate = nums[i];
+7            }
+8            if(nums[i]==candidate){
+9                count++;
+10            }
+11            else{
+12                count--;
+13            }
+14        }
+15        return candidate;
+16    }
+17}
