cd8ad47c2e6647367439949b5629178bc6a00153
Lokesh Chaudhary <156283625+chlokeshoct2006@users.noreply.github.com>
2026-07-13 13:18:59 +0530

    File updated at 7/13/2026, 1:18:45 PM

--- 724-FindPivotIndex/724-FindPivotIndex.java
+++ 724-FindPivotIndex/724-FindPivotIndex.java
@@ -1,23 +1,20 @@
-// Last updated: 7/13/2026, 1:05:48 PM
+// Last updated: 7/13/2026, 1:18:45 PM
 1class Solution {
-2    public int pivotIndex(int[] nums) {
-3        int n = nums.length;
-4        int[] leftSum = new int[n];
-5        int[] rightSum = new int[n];
-6        leftSum[0] = nums[0];
-7        for(int i=1; i<n; i++){
-8            leftSum[i] = leftSum[i-1]+nums[i];
+2    public int thirdMax(int[] nums) {
+3        List<Integer> list = new ArrayList<>();
+4        
+5        for(int i=0; i<nums.length; i++) {
+6            if(!list.contains(nums[i])) {
+7                list.add(nums[i]);
+8            }
 9        }
-10        rightSum[n-1] = nums[n-1];
-11        for(int i=n-2; i>=0; i--){
-12            rightSum[i] = rightSum[i+1]+nums[i];
-13        }
-14
-15        for(int i=0; i<n; i++){
-16            if(leftSum[i]==rightSum[i]){
-17                return i;
-18            }
-19        }
-20        return -1;
-21    }
-22}
+10
+11        Collections.sort(list);
+12        
+13        if(list.size() < 3) {
+14            return list.get(list.size()-1);
+15        } else {
+16            return list.get(list.size()-3);
+17        }
+18    }
+19}
